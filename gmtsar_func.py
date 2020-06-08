63d0d72e5a34062e1d2f4cd30e9dc830143e678d
Eric Lindsey <ericl256@gmail.com>
2020-06-08 21:00:24 +0800

    add plot_created_intfs function

--- gmtsar_func.py
+++ gmtsar_func.py
@@ -444,6 +444,14 @@ def setup_intf(SAT,dataDotIn,intf_file,intf_config,lines=None,no_label=False):
             f.write('%s:%s\n'%(intflist[i][0],intflist[i][1]))
     #plot scenes, and intflist / donelist as lines
     #plot_intfs(intflist,donelist,scenelist,decyears,baselines,lines=lines,no_label=no_label)
+    #gmt version
+    plot_created_intfs(max_timespan, max_baseline, SAT)
+
+def plot_created_intfs(max_timespan, max_baseline, SAT):
+    baselinetable='raw/baseline_table.dat'
+    plot_intfs_cmd = " %s/plot_created_intfs.csh %s %d %d %s"%(cshpath,baselinetable,max_timespan,max_baseline,SAT)
+    print('%s'%plot_intfs_cmd)
+    run_command(plot_intfs_cmd, logging=False)
 
 
 # def plot_intfs(intflist,donelist,scenelist,decyears,baselines,lines=None,no_label=False):    
