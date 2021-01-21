aec7f0c469f09571b81c91955ac3d09801d7fe59
Eric Lindsey <ericl256@gmail.com>
2021-01-21 13:53:28 -0700

    new setup function to add GMTSAR_APP to environment

---
+++ setup_gmtsar_app.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+here=`pwd`
+echo adding GMTSAR_APP variable to environment. Add this line to your .bashrc or .zshrc to use automatically:
+echo export GMTSAR_APP=\"$here\"
+export GMTSAR_APP="$here"
+
